aa9addfe4b551819751791a93ec2caa297ed0abb
MMAT15 <124900087+MMAT15@users.noreply.github.com>
2024-12-22 14:13:10 -0300

    pipi

--- iniciar.js
+++ iniciar.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const password = document.getElementById("password").value.trim();
   
       try {
-        const response = await fetch("http://localhost:3000/login", {
+        const response = await fetch("http://localhost:4000/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email, password }),
