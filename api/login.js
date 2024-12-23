dffc0c0f2c43bf839da221d06cf60edbe7bdc3f0
MMAT15 <124900087+MMAT15@users.noreply.github.com>
2024-12-23 12:09:11 -0300

    mejorias

---
+++ api/login.js
@@ -0,0 +1,19 @@
+import { readUsers } from './utils';
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    const { email, password } = req.body;
+    const users = readUsers();
+
+    const user = users.find((user) => user.email === email && user.password === password);
+
+    if (!user) {
+      return res.status(401).json({ message: 'Credenciales inválidas.' });
+    }
+
+    return res.status(200).json({ message: 'Inicio de sesión exitoso.', username: user.username });
+  }
+
+  res.setHeader('Allow', ['POST']);
+  res.status(405).end(`Método ${req.method} no permitido`);
+};
